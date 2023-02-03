df656b07f74e1af2e14d26eb06b38d7303b5613c
Vinicius <viniciuscastrogs@gmail.com>
2023-02-03 18:25:59 -0300

    Project created

---
+++ src/main/java/com/cursospring/CursospringNelioalvesApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringNelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringNelioalvesApplication.class, args);
+	}
+
+}
